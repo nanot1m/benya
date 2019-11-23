62da9cb8e0884a3b41a88371c1e2498c590a4d1b
hazam <hazam@yandex-team.ru>
2019-11-23 21:51:47 +0500

    Updates

---
+++ precache-manifest.d9a01e86f36c697903e44b9e43da01b4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e4b18a7120e939212749d86974c32f0",
+    "url": "/benya/index.html"
+  },
+  {
+    "revision": "e42178a5072708149098",
+    "url": "/benya/static/css/2.f2caa9bb.chunk.css"
+  },
+  {
+    "revision": "ea8bd7507d24f2fc9bbc",
+    "url": "/benya/static/css/main.948fa6c0.chunk.css"
+  },
+  {
+    "revision": "e42178a5072708149098",
+    "url": "/benya/static/js/2.79a28446.chunk.js"
+  },
+  {
+    "revision": "ea8bd7507d24f2fc9bbc",
+    "url": "/benya/static/js/main.d2ccdcb6.chunk.js"
+  },
+  {
+    "revision": "f7bc45defe2b72c43c15",
+    "url": "/benya/static/js/runtime-main.8482648c.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/benya/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/benya/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/benya/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/benya/static/media/slick.f97e3bbf.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f8526880464a5e72031e0bf5bb9067c9.js"
+  "/benya/precache-manifest.d9a01e86f36c697903e44b9e43da01b4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/benya/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
